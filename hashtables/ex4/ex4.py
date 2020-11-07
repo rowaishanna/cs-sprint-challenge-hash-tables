1aeac0b3e5995506f45f37a980f7ddb2b82224ab
rowaishanna <rowaishanna@gmail.com>
2020-11-06 22:29:57 -0500

    completed ex4,5

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,25 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
+    d = dict()
+    # loop through the list
+    for value in a:        
+        # check to see if the value is positive
+        if value > 0:
+            # if the value is not in the dictionary
+            if value not in d:
+                # add it to the dictionary
+                d[value] = 1  
+    # create an empty list to store the answers
+    result = []
+    # loop through the inverse of the list
+    a = [-1*(value) for value in a]
+    for value in a:
+        # if the inverted value is in our dictionary
+        if value in d:
+            # add it to our list
+            result.append(value)
+    # return the list
     return result
 
 
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,32 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    d = dict()
+    # loop through the files
+    for path in files:
+        # split the path at /
+        path2 = path.split('/')
+        # loop through path
+        for word in path2:
+            # if the word is not in the dictionary
+            if word not in d:
+                # create a list with the path in it
+                l = [path]
+                # add it to the dict
+                d[word] = l
+            # if the word is in the dictionary
+            else:
+                # append the path to the existing list
+                d[word].append(path)
+    
+    # create a list to hold our results
+    result = []
+    # loop through queries
+    for querie in queries:
+        if querie in d:
+            # loop through the paths
+            for path in d[querie]:
+                result.append(path)
 
     return result
 
