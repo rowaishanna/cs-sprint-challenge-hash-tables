9f58b84a6379d9197cfa51c34048528b8e7772cf
rowaishanna <rowaishanna@gmail.com>
2020-11-06 21:17:47 -0500

    completed ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,23 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    d = dict()
+    route = []
+    # loop through the tickets
+    for ticket in tickets:
+        # set each key to the source and each value to the destination
+        d[ticket.source] = ticket.destination
+    
+    # find the first flight
+    ff = d['NONE']
+    # add the first flight to the route
+    route.append(ff)
+    # loop through until we find the last flight
+    while d[ff] is not 'NONE':
+        # append the value to the route
+        route.append(d[ff])
+        # update ff
+        ff = d[ff]
+    # append the final trip to the list
+    route.append(d[ff])
     return route
